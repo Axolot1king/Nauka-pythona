7ecfd7540417796145b133a8804fe6f7d7b07590
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-01-23 19:38:45 +0100

    Please

---
+++ L2-variables/Lesson_numbero_doso.py
@@ -0,0 +1,19 @@
+var1 = 'jakiś tam text'
+var2 = '6'
+var3 = 6
+var4 = 0.5
+
+# konkatonacja
+# TODO wyświetlić var1 + var1 tak żeby dało się to odczytać
+print(var1 + ', ' + var1)
+# print(var1 + var3) - nie zadziała ze względu na konflikt zmiennych
+print(var1 + var2)
+print((var1 + ', ') * var3)
+print(var3 * var4)
+
+varMożeszNazwaćJakChcesz = var2 + var2
+
+print(varMożeszNazwaćJakChcesz)
+var2 = var3 + var4 + 9
+print (var2)
+
