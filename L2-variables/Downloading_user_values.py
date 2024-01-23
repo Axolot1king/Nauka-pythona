618f711726b4d84d91826ebb5400e50e2fdab82a
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-01-23 19:55:42 +0100

    moja pierwsza (nie działającza) strona

---
+++ L2-variables/Downloading_user_values.py
@@ -0,0 +1,8 @@
+var1 = '(czerwoną, białą): '
+var2 = '(5, 10, 15): '
+name = input('Jaki typ cegły chciałbyś/ałabyś mieć na ścianie' + var1)
+amount = input('Jaką ilość pan/i chciał/a by pan/i cegły' + var2)
+
+# TODO funkcja wyświetlająca użytkownikowi zamuwienie/a
+
+print('Zamówiłeś/aś ' + name + 'w ilości ' + amount)
