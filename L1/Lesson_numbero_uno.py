5e2b1b18a5a590fe597d74d35c4c93b2b10dcedb
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-01-23 18:51:26 +0100

    I finally do something Bready Eggy GO!!

---
+++ L1/Lesson_numbero_uno.py
@@ -0,0 +1,19 @@
+print((2 + 2) * 2)
+print(1 + 2)
+print(1 - 2)
+print(1 * 2)
+print(1 / 2)
+print(9 % 2)
+print(10 // 3)
+print(3 ** 2)
+
+# test komentarza jednolinijkowego
+"""
+hleb z masłem
+jest tani
+i fajny
+"""
+
+print('bready')
+print('bready ' * 3)
+
