dcb417e98a8c41ed962b5c7bbad7fae8a8f5b8ec
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-02-01 18:38:40 +0100

    lesson 3 theory is end

--- L3-instrukcje_warunkowe/L3_-_nazwij_jak_chcesz.py
+++ L3-instrukcje_warunkowe/L3_-_nazwij_jak_chcesz.py
@@ -1,3 +1,4 @@
+"""
 varq = 5
 varw = 5
 
@@ -6,3 +7,31 @@ print(varw == varq)
 print(varw != varq)
 print(varw >= varq)
 print(varw < varq)
+"""
+
+zm1 = True
+zm2 = False
+"""
+zm1 = 2
+zm2 = zm1
+
+if zm2 != zm1:
+    print('cokolwiek abcd')
+    print('cokolwiek abcd')
+
+print('cokolwiek abcd')
+"""
+
+wiek = input( 'enter your age: ')
+wiek = int(wiek)
+zm2 = zm1
+
+if wiek > 16:
+    print( 'możesz wejść na stronę =)')
+
+elif wiek < 16:
+    print('jesteś zbyt młody =(')
+
+else:
+    print( 'masz 16 lat! chyba bardzo chciałeś wejść na ta stronę=)')
+
