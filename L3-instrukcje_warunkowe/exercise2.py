d3c2d065b2aa246b3063b7a6db93bf33bf3d1d68
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-02-11 15:23:39 +0100

    ibfhgbihfdhgfg

--- L3-instrukcje_warunkowe/exercise2.py
+++ L3-instrukcje_warunkowe/exercise2.py
@@ -17,14 +17,64 @@ else:
     print('you need to wait ' + str(18 - wk) + 'years')
 if wk < 21:
     print('you cannot apply for the position of MP')
+    print('you need to wait ' + str(21 - wk) + 'years')
 
 else:
     print('you can apply for the position of MP!')
 
+if wk >= 35:
+    print('you can run for president')
+
+else:
+    print("you can't run for president")
+    print('you need to wait ' + str(35 - wk) + 'years')
+
 if wk >= 60:
     if pl == 'F':
-        print('możesz ubiegać się o emeryturę!')
+        print('you can apply for a pension!')
+
+else:
+    print('you need to wait ' + str(60 - wk) + 'years')
 
 if wk >= 65:
     if pl == 'M':
-        print('możesz ubiegać się o emeryturę!')
+        print('you can apply for a pension!')
+
+else:
+    print('you need to wait ' + str(65 - wk) + 'years')
+
+prw = input('do u have driving licence '
+            '\nof category A2 at least 2 '
+            '\nyears? (yes/no):')
+
+if prw == 'yes':
+    if wk >= 20:
+        print('you can do driving licence '
+              '\nof category A')
+    else:
+        print("you can't do driving licence of category A ")
+        print('you need to wait ' + str(20 - wk) + 'years')
+
+if prw == 'no':
+    if wk >= 24:
+        print('you can do driving licence '
+              '\nof category A')
+    else:
+        print("you can't do driving licence of category A ")
+        print('you need to wait ' + str(24 - wk) + 'years')
+
+if wk >= 18:
+    print('you can do driving licence of category B')
+else:
+    print("you can't do driving licence of category B")
+    print('you need to wait ' + str(18 - wk) + 'years')
+
+if wk >= 16:
+    print('you can do driving licence of category B1')
+else:
+    print("you can't do driving licence of category B1")
+    print('you need to wait ' + str(16 - wk) + 'years')
+
+
+
+
