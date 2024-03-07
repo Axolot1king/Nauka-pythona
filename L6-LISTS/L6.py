953f553d21904241ab39aa20fc18a4eb6315ef72
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-03-07 18:20:49 +0100

    did the L5 lesson to the end =)

---
+++ L6-LISTS/L6.py
@@ -0,0 +1,16 @@
+dowolna = 3465
+dow = 0
+str = 'słowo'
+lists = [9854, 'fod', 75, 'fud', 65, dowolna]
+print(lists)
+print(lists[1])
+lists[4] = lists[0]
+print(lists)
+print(str[1], [6, 5])
+print(lists * 2)
+
+while dow <= len(lists):
+    print(lists[dow])
+    dow += 1
+
+
