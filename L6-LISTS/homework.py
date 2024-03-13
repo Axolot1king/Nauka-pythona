f53070391f02852cf9202009daa02d02a93f2fef
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-03-13 17:06:29 +0100

    DONE! a little bit out of time but works

--- L6-LISTS/homework.py
+++ L6-LISTS/homework.py
@@ -1,20 +1,7 @@
-dow = 0
-listasa = ['S', 'n', 'i', 'z', 'z', 'l', 'e', 'p', 'o', 'p', ' ']
-# String już sam w sobie jest listą, nie musisz tego tak zapisywać. Popraw na zwykł string
+lista = 'Pneumonoultramicroscopicsilicovolcanoconiosi'
+x = 0
+lel = len(lista)
 
-while dow <= 2:
-# Nie musisz określać warunku zamknięcia pętli na podstawie wyliczania, odpowiedniej liczby znaków w stringu, masz od tego odpowiednie polecenie, które zrobi to za Ciebie i nim się posłuż w tym wypadku.
-    print(listasa[0])
-    print(listasa[1])
-    print(listasa[2])
-    print(listasa[3])
-    print(listasa[4])
-    print(listasa[5])
-    print(listasa[7])
-    print(listasa[8])
-    print(listasa[9])
-    print(listasa[10])
-    dow += 1
-
-# Używasz pętli właśnie po to, aby nie musieć tyle razy pisać printa...
-# Napisz długie zdanie, składające się z 40 znaków minimum i ogranicz program do jednego printa w pętli.
+while x <= 44:
+    print(lista[x])
+    x += 1
