093a4564a043db01e3b6d44449a3153754e36ad8
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-03-14 17:57:44 +0100

    DONE! a little bit out of time but works

--- L6-LISTS/L6.py
+++ L6-LISTS/L6.py
@@ -1,16 +1,23 @@
 dowolna = 1
 dow = 0
-str = 'słowo'
+strt = 'słowo'
 lists = [9854, 'fod', 75, 'fud', 65, dowolna]
 print(lists)
 print(lists[1])
 lists[4] = lists[0]
 print(lists)
-print(str[1], [6, 5])
+print(strt[1], [6, 5])
 print(lists * 2)
 
-while dow <= len(lists):
+while dow < len(lists):
     print(lists[dow])
     dow += 1
 
+lists.append([991, 'io9'])
+print(lists[6][0])
+print(lists[6][1])
+
+lists.insert(6,strt)
+ugaboga = lists.index(75)
+print(ugaboga)
 
---
+++ L6-LISTS/L6v2.py
@@ -0,0 +1,17 @@
+lists1 = [9854, 56, 75, 99, 65, 2]
+
+print('minimalna wartość to :' + str(min(lists1)))
+print('maxymalna wartość to :' + str(max(lists1)))
+
+lists1.sort()
+
+print(lists1)
+
+lists1.reverse()
+
+print(lists1)
+
+lists1.clear()
+
+print(lists1)
+
