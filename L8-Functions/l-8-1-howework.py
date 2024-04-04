54bcd4ad1f7934e519595511f9a5eefe67de58d4
filip8123 <114266409+filip8123@users.noreply.github.com>
2024-04-04 17:02:16 +0200

    DidIt

--- L8-Functions/l-8-1-howework.py
+++ L8-Functions/l-8-1-howework.py
@@ -5,12 +5,10 @@ def print_vertical(word):
     for letter in word:
         print(letter)
 
-# Main function
 def main():
     input_word = get_input()
     print("Printing word vertically:")
     print_vertical(input_word)
 
-# Call the main function to execute the program
 if __name__ == "__main__":
     main()
